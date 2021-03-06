0e3ac1f6d58a4baa5a814746a0f899bf37c42494
Martin Moy <martinmoy1991@gmail.com>
2021-03-06 08:24:10 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,19 +9,19 @@ class BankAccount
   end
 
   def deposit(amount)
-    self.balance += amount
+    @balance += amount
   end
 
   def display_balance
-    "Your balance is $#{self.balance}."
+    "Your balance is $#{@balance}."
   end
 
   def valid?
-    self.balance > 0 && self.status == "open" ? true : false
+    @balance > 0 && @status == "open" ? true : false
   end
 
   def close_account
-    self.status = "closed"
+    @status = "closed"
   end
 
 end
